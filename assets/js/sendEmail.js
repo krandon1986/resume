646f3fae0f97cdc776b8e60acc01430cb67bb338
krandon1986 <krandon1986@yahoo.co.uk>
2023-05-08 16:15:57 +0000

    Sending emails

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+function sendMail(contactForm) {
+    emailjs.send("service_a0cdonp","template_ci3vdce", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
+        return false;
+}
